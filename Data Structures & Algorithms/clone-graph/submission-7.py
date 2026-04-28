19a0be9b23a8f9326d60bf9538029e9ff4db07fe
jadenho10 <114277009+jadenho10@users.noreply.github.com>
2026-04-28 15:25:31 -0700

    Add: clone-graph - submission-7

---
+++ Data Structures & Algorithms/clone-graph/submission-7.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        oldToNew = {} # want 1:1 mapping of og to copy
+        
+        copy = Node(node.val, None)
+        oldToNew[node] = copy
+
+        # BFS algorithm
+        q = deque([node])
+        while q:
+            n = q.popleft()
+            for nei in n.neighbors:
+                if nei not in oldToNew:
+                    q.append(nei)
+                    oldToNew[nei] = Node(nei.val)
+                oldToNew[n].neighbors.append(oldToNew[nei])
+        return oldToNew[node]
+
+
